4c284bb08d369f807dcf0d47f0ef326c29247d7a
Carlos Castro <alu0100819847@ull.edu.es>
2017-02-25 17:16:45 +0000

    Añadida seccion al gulpfile para automatizar el deploy #4

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,13 @@ var ghPages = require('gulp-gh-pages');
 gulp.task('build', shell.task([
 'gitbook build txt html'
 ]));
+
+gulp.task('deploy', function() {
+return gulp.src('./html/**/*')
+.pipe(ghPages());
+});
+
+gulp.task('deploy&push', ['deploy'], function() {
+  return gulp.src('./html/**/*')
+  .pipe(ghPages(['git@github.com:alu0100819847/Repo-Ayuda-DSI.git']))
+});
